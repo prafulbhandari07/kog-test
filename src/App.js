c0b36a323b8f11ea5049c1d84d3bfd4d332c5f11
prafulbhandari07 <pbhandari490@gmail.com>
2023-12-06 18:49:50 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div id='cont'>
+
+        <p>LEARN MORE ABOUT KOGNICS</p>
+        <p>Leverage the power of AI to unleash your company's potential!</p>
+        <div className='card'>
+          <div id='child'>
+            <p id='demo'>Get a Demo
+            </p>
+            <p>
+              To see how Kognics works and how it's going to increase your business's worth.
+            </p>
+            <form className='form'>
+              <p>Name:</p>
+              <input type='text'></input>
+              <p>Email ID:</p>
+              <input type='text'></input>
+              <p>Company Name:</p>
+              <input type='text'></input>
+              <button >Request a Demo</button>
+            </form>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
